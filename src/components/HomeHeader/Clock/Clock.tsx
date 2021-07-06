240b0941334d6c68d1592bc49f462a585fb89561
Jake Robins <me@jakerobins.com>
2021-07-06 15:29:26 -0600

    restyle clock for mui

--- src/components/HomeHeader/Clock/Clock.tsx
+++ src/components/HomeHeader/Clock/Clock.tsx
@@ -1,24 +1,75 @@
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, makeStyles, Theme, Typography } from "@material-ui/core";
 import { useCurrentTime } from "../../../contexts/CurrentTimeContext";
-import styles from "./styles/Clock.module.css";
+import classnames from "clsx";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 export type ClockProps = {};
 
+const useStyles = makeStyles((theme: Theme) => ({
+  clock: {
+    fontFamily: ["Oxygen Mono", "monospace"].join(","),
+  },
+  clockPrimary: {
+    fontSize: "4.4rem",
+    [theme.breakpoints.up(415)]: {
+      fontSize: "6.3rem",
+    },
+  },
+  clockSecondary: {
+    fontSize: "1.5rem",
+    [theme.breakpoints.up(415)]: {
+      fontSize: "2.2rem",
+    },
+  },
+  marginBottom: {
+    marginBottom: "8px",
+  },
+}));
+
 export default function Clock(props: ClockProps) {
+  const classes = useStyles();
+  const isWideScreen = useMediaQuery("(min-width:415px)");
+
   const { getLMST, ls, year } = useCurrentTime();
   const { hour, min, sec } = getLMST();
 
   const roundedLs = Math.round(ls * 10000) / 10000;
 
   return (
-    <Grid container>
-      <Grid item xs={10}>
-        <Typography>
+    <Grid container spacing={2}>
+      <Grid item xs={9}>
+        <Typography
+          component="span"
+          className={classnames(classes.clock, classes.clockPrimary)}
+        >
           {hour}:{min}
         </Typography>
       </Grid>
-      <Grid item xs={2}>
-        <Typography>:{sec}</Typography>
+      <Grid
+        container
+        item
+        xs={3}
+        direction="column"
+        justifyContent="center"
+        alignItems="flex-end"
+        className={classes.marginBottom}
+      >
+        <Grid item>
+          <Typography
+            component="span"
+            className={classnames(classes.clock, classes.clockSecondary)}
+          >
+            :{sec}
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography
+            component="span"
+            className={classnames(classes.clock, classes.clockSecondary)}
+          >
+            MST
+          </Typography>
+        </Grid>
       </Grid>
     </Grid>
     // <div className={styles.root}>
--- src/components/HomeHeader/HomeHeader.tsx
+++ src/components/HomeHeader/HomeHeader.tsx
@@ -1,23 +1,33 @@
 import Clock from "./Clock/Clock";
-import { Button, Typography, Container } from "@material-ui/core";
+import { Button, Grid, Typography, Container } from "@material-ui/core";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { useTheme } from "@material-ui/core/styles";
 
 export type HomeHeaderProps = {};
 
 export default function HomeHeader(props: HomeHeaderProps) {
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up("sm"));
+  const isWideScreen = useMediaQuery("(min-width:415px)");
 
   return (
     <Container component="header" maxWidth="xs" disableGutters>
-      <Typography variant={matches ? "h1" : "h2"} component="h1" align="center">
-        MARSTI.ME
-      </Typography>
-      <Clock />
-      <Button variant="contained" color="primary" href="#faq" size="large">
-        What do these mean?
-      </Button>
+      <Grid container direction="column" alignItems="center">
+        <Grid item>
+          <Typography
+            variant={isWideScreen ? "h1" : "h2"}
+            component="h1"
+            align="center"
+          >
+            MARSTI.ME
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Clock />
+        </Grid>
+        <Grid item>
+          <Button variant="contained" color="primary" href="#faq" size="large">
+            What do these mean?
+          </Button>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
