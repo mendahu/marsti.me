3913964de6f31e0c7b51c058ce3a4cbdbcb19b45
Jake Robins <me@jakerobins.com>
2021-07-07 16:03:16 -0600

    refactor Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import DateFnsUtils from "@date-io/date-fns";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { useTime } from "../src/hooks/useTime";
 import CurrentTimeProvider from "../src/contexts/CurrentTimeContext";
+import Head from "next/head";
 
 const oswaldFontFamily = {
   fontFamily: ["Oswald", "sans-serif"].join(","),
@@ -47,6 +48,22 @@ function MyApp({ Component, pageProps }) {
       <CssBaseline />
       <CurrentTimeProvider value={useTime()}>
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <Head>
+            <title>Mars Time</title>
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Oswald&display=swap"
+              rel="stylesheet"
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Oxygen+Mono&display=swap"
+              rel="stylesheet"
+            />
+            <link
+              rel="stylesheet"
+              href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            />
+          </Head>
           <Component {...pageProps} />
         </MuiPickersUtilsProvider>
       </CurrentTimeProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import HomeHeader from "../src/components/HomeHeader/HomeHeader";
 import MissionCard from "../src/components/MissionCard/MissionCard";
 import { useMissionCoords } from "../src/hooks/useMissionCoords";
@@ -30,48 +29,30 @@ export default function Home() {
   const coords = useMissionCoords(spacecraft as Spacecraft[]);
 
   return (
-    <>
-      <Head>
-        <title>Mars Time</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Oswald&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Oxygen+Mono&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-      </Head>
-      <Container maxWidth="md">
-        <HomeHeader />
-        <Grid
-          container
-          spacing={3}
-          justifyContent="center"
-          className={classes.grid}
-        >
-          {spacecraft.map((vehicle) => {
-            const { id } = vehicle;
-            return (
-              <MissionCard
-                key={vehicle.id}
-                name={vehicle.name}
-                lat={coords[id].lat}
-                lon={coords[id].lon}
-                missionStart={new Date(vehicle.epoch)}
-                bannerUrl={vehicle.banner}
-              />
-            );
-          })}
-        </Grid>
-        <DateConverter />
-        {/*<LearnSection /> */}
-      </Container>
-    </>
+    <Container maxWidth="md">
+      <HomeHeader />
+      <Grid
+        container
+        spacing={3}
+        justifyContent="center"
+        className={classes.grid}
+      >
+        {spacecraft.map((vehicle) => {
+          const { id } = vehicle;
+          return (
+            <MissionCard
+              key={vehicle.id}
+              name={vehicle.name}
+              lat={coords[id].lat}
+              lon={coords[id].lon}
+              missionStart={new Date(vehicle.epoch)}
+              bannerUrl={vehicle.banner}
+            />
+          );
+        })}
+      </Grid>
+      <DateConverter />
+      {/*<LearnSection /> */}
+    </Container>
   );
 }
