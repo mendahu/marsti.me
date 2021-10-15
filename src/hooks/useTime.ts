2359deb289f55950f6e3b2e29f7aeda9e787814f
Jake Robins <me@jakerobins.com>
2021-10-15 14:17:00 -0500

    add types for useTime context

--- src/hooks/useTime.ts
+++ src/hooks/useTime.ts
@@ -1,7 +1,20 @@
 import { MarsDate } from "mars-date-utils";
 import { useEffect, useState } from "react";
 
-export const useTime = () => {
+type Time = {
+  hour: string;
+  min: string;
+  sec: string;
+};
+
+export type TimeUtilities = {
+  getLMST: (lon?: number) => Time;
+  getLTST: (lon?: number) => Time;
+  year: number;
+  ls: number;
+};
+
+export const useTime = (): TimeUtilities => {
   const [cd, setCd] = useState<MarsDate>(new MarsDate(new Date()));
 
   const year = cd.getCalendarYear();
@@ -17,7 +30,7 @@ export const useTime = () => {
     }, 1000);
   }, []);
 
-  const generateTimeObj = (time: string) => {
+  const generateTimeObj = (time: string): Time => {
     return {
       hour: time.slice(0, 2),
       min: time.slice(3, 5),
@@ -25,12 +38,12 @@ export const useTime = () => {
     };
   };
 
-  const getLMST = (lon: number = 0) => {
+  const getLMST = (lon: number = 0): Time => {
     const mst = cd.getLMST(lon);
     return generateTimeObj(mst);
   };
 
-  const getLTST = (lon: number = 0) => {
+  const getLTST = (lon: number = 0): Time => {
     const tst = cd.getLTST(lon);
     return generateTimeObj(tst);
   };
