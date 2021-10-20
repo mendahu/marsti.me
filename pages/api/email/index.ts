81afabd8fda03b0beae1b0d1fb246d2a07a69ddd
Jake Robins <me@jakerobins.com>
2021-10-20 12:40:55 -0500

    add variable ingestion to template

--- pages/api/email/index.ts
+++ pages/api/email/index.ts
@@ -3,6 +3,7 @@ import nodemailer from "nodemailer";
 import auth from "basic-auth";
 import compare from "tsscmp";
 import fs from "fs";
+import tsp from "templatestringparser";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   // Reject non-POST requests
@@ -46,26 +47,41 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
     },
   });
 
-  // Fetch Email Template
-  const template = new Promise((resolve, reject) => {
-    const html = fs.readFile(
-      "assets/birthday-email.html",
-      "utf8",
-      (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      }
-    );
-  });
-
-  // Send Email
+  // Fetch email template variables
   const { email } = req.body;
   const fromEmail = user;
 
-  return template
+  const templateVars = {
+    earthBirthday: "January 1st, 1990",
+    ls: {
+      value: "blurp",
+      season: "glorp",
+    },
+    my: "11",
+    marsAge: {
+      abs: "12",
+      spoken: "13",
+    },
+    nextMarsBirthday: {
+      date: "14",
+      age: "15",
+    },
+  };
+
+  // Fetch Email Template
+  const fetchHTMLTemplate = new Promise((resolve, reject) => {
+    fs.readFile("assets/birthday-email.html", "utf8", (err, template) => {
+      if (err) {
+        reject(err);
+      } else {
+        const injectedTemplate = tsp(template, templateVars);
+        resolve(injectedTemplate);
+      }
+    });
+  });
+
+  // Send Email
+  return fetchHTMLTemplate
     .then((html) => {
       return transporter.sendMail({
         from: `Marsti.me <${fromEmail}>`,
