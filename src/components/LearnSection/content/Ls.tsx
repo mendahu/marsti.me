5e70c281da42834a24cae815e1790091eea6fdd9
Jake Robins <me@jakerobins.com>
2021-11-02 08:29:20 -0600

    rewrite Ls section

--- src/components/LearnSection/content/Ls.tsx
+++ src/components/LearnSection/content/Ls.tsx
@@ -1,3 +1,12 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import styles from "./styles/Ls.module.css";
 
 export type LsProps = {};
@@ -5,13 +14,58 @@ export type LsProps = {};
 export default function Ls(props: LsProps) {
   return (
     <div>
-      <p>
+      <Typography paragraph>
         Solar Longitude measures the point where Mars is in its orbit around the
         sun. It's a numerical representation of seasons, since we don't track
-        months on Mars. Solar Longitude (Ls, said out loud 'ell sub ess') starts
-        at 0 (Vernal Equinox in the Northern Hemisphere) and goes to 360 degrees
-        one years later.
-      </p>
+        months on Mars.
+      </Typography>
+      <Typography paragraph>
+        Solar Longitude (L<sub>s</sub>, said out loud <i>ell sub ess</i>) starts
+        at 0&deg; (the beginning of Spring, or Vernal Equinox, in the Northern
+        Hemisphere) and goes to 360&deg; degrees one year later.
+      </Typography>
+      <Typography>
+        Here are some L<sub>s</sub> numbers and their cooresponding seasons.
+      </Typography>
+
+      <Table sx={{ maxWidth: 300 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              L<sub>s</sub>
+            </TableCell>
+            <TableCell>Northern Hemisphere</TableCell>
+
+            <TableCell>Approx. Earth date</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell>0&deg;</TableCell>
+            <TableCell>First day of Summer</TableCell>
+
+            <TableCell>Jun.</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>90&deg;</TableCell>
+            <TableCell>First day of Fall</TableCell>
+
+            <TableCell>Sep.</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>180&deg;</TableCell>
+            <TableCell>First day of Winter</TableCell>
+
+            <TableCell>Dec.</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>270&deg;</TableCell>
+            <TableCell>First day of Spring</TableCell>
+
+            <TableCell>Mar.</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 }
