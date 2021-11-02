fb9d57f4d7cb1e00b3a3ffa4a6dfc360cd792fff
Jake Robins <me@jakerobins.com>
2021-11-02 10:59:00 -0600

    fix bug with birthday validation

--- src/components/BirthdayTool/BirthdayTool.tsx
+++ src/components/BirthdayTool/BirthdayTool.tsx
@@ -61,6 +61,7 @@ export default function BirthdayTool() {
                 label="Earth Birthday"
                 value={earthBirthday}
                 onChange={setEarthBirthday}
+                maxDateTime={new Date()}
               />
             </Box>
             {earthBirthday && (
