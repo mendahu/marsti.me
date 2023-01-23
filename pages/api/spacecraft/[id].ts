bca39e95d05f929592edb5ee61ff2c65cd886791
Jake Robins <me@jakerobins.com>
2023-01-23 14:41:52 -0600

    add images to api

--- pages/api/spacecraft/[id].ts
+++ pages/api/spacecraft/[id].ts
@@ -2,6 +2,19 @@ import { MarsDate } from "mars-date-utils";
 import { NextApiRequest, NextApiResponse } from "next";
 import spacecraft from "../../../config/spacecraft.json";
 import { formatLs } from "../../../src/helpers/formatLs";
+import zhurongBanner from "../../../public/zhurong_banner.png";
+import ingenuityBanner from "../../../public/ingenuity_banner.png";
+import perseveranceBanner from "../../../public/perseverance_banner.png";
+import mslBanner from "../../../public/msl_banner.png";
+import inSightBanner from "../../../public/insight_banner.png";
+
+const banners = {
+  igy: ingenuityBanner,
+  msl: mslBanner,
+  m20: perseveranceBanner,
+  zhu: zhurongBanner,
+  ins: inSightBanner,
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // Reject non-GET requests
@@ -20,6 +33,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   const response = {
     spacecraft: vehicle,
+    bannerUrl: `https://www.marsti.me` + banners[vehicle.id].src,
     currentSol: Math.floor(
       new MarsDate(new Date(vehicle.epoch)).getAgeInSols()
     ),
