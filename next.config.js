409f86ee3f7f10c8a715128c57dc7bb043c10fbc
Jake Robins <me@jakerobins.com>
2023-01-23 16:36:03 -0600

    update config path

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/spacecraft/",
+        source: "/api/spacecraft/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            value: "GET",
           },
           {
             key: "Access-Control-Allow-Headers",
