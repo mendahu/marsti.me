c553f34ca04574d65c31900c72d6ed233cebf9da
Jake Robins <me@jakerobins.com>
2023-01-23 16:02:31 -0600

    fix path for headers

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/spacecraft*",
+        source: "/api/spacecraft/",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
